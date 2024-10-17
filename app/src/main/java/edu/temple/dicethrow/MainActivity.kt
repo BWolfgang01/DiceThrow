1f8487b164081f21e2af9156d6715d8f5e2eb1b7
BWolfgang01 <tun39109@temple.edu>
2024-10-17 12:17:29 -0400

    Fixed rotation bug

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -2,19 +2,22 @@ package edu.temple.dicethrow
 
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val newDie100 = DieFragment.newInstance(100)
+
+        if (supportFragmentManager.findFragmentById(R.id.dieContainer) !is DieFragment){
+            supportFragmentManager.beginTransaction().add(R.id.dieContainer, newDie100).commit()
+        }
+
         findViewById<Button>(R.id.rollDiceButton).setOnClickListener{
             (supportFragmentManager
-                .findFragmentById(R.id.DieContainer) as DieFragment).throwDie()
+                .findFragmentById(R.id.dieContainer) as DieFragment).throwDie()
         }
     }
 
